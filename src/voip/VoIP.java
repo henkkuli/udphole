24b74bb04ad1dadf936cc8a7f812848c5f6c35c8
Henrik <Henrik@10.0.0.8>
2013-08-05 23:52:49 +0300

    VoIP now open connection

--- src/voip/VoIP.java
+++ src/voip/VoIP.java
@@ -1,7 +1,28 @@
-
 package voip;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import udphole.Hole;
 
 public class VoIP {
-    
+
+    public VoIP(String serverName, int serverPort, String pool) {
+        try {
+            Hole hole = new Hole(InetAddress.getByName(serverName), serverPort, pool);
+            
+            
+        } catch (IOException ex) {
+            Logger.getLogger(VoIP.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void main(String args[]) {
+        if (args.length < 3) {
+            System.exit(-1);
+        }
+        new VoIP(args[0], Integer.parseInt(args[1]), args[2]);
+    }
 }
